6c852bfd03cb5eb1593d983841cdf08a250749cc
George Heller <georgeheller8@gmail.com>
2026-09-07 18:59:36 -0700

    Update dispatch table

--- I8080.c
+++ I8080.c
@@ -63,30 +63,30 @@ void increment_cycles(I8080* cpu, uint8_t num) {
 
 const void (*dispatch[256])(I8080* cpu) = 
 {
-    NOP, LXI, STAX, INX, INR, DCR, MVI, RLC, NOP, DAD, LDAX, DCX, INR, DCR, MVI, RRC,
-    NOP, LXI, STAX, INX, INR, DCR, MVI, RAL, NOP, DAD, LDAX, DCX, INR, DCR, MVI, RAR,
-    NOP, LXI, SHLD, INX, INR, DCR, MVI, DAA, NOP, DAD, LHLD, DCX, INR, DCR, MVI, CMA,
-    NOP, LXI, STA,  INX, INR, DCR, MVI, STC, NOP, DAD, LDA,  DCX, INR, DCR, MVI, CMC,
-    [0x40 ... 0x7f] = MOV,
-    [0x76] = HLT,
-    [0x80 ... 0x87] = ADD,
-    [0x88 ... 0x8F] = ADC,
-    [0x90 ... 0x97] = SUB,
-    [0x98 ... 0x9F] = SBB,
-    [0xA0 ... 0xA7] = ANA,
-    [0xA8 ... 0xAF] = XRA,
-    [0xB0 ... 0xB7] = ORA,
-    [0xB8 ... 0xBF] = CMP,
-    RNZ, POP, JNZ, JMP,  CNZ, PUSH, ADI, RST, RZ,  RET,  JZ,  JMP,  CZ,  CALL, ACI, RST,
-    RNC, POP, JNC, OUT,  CNC, PUSH, SUI, RST, RC,  RET,  JC,  IN,   CC,  CALL, SBI, RST,
-    RPO, POP, JPO, XTHL, CPO, PUSH, ANI, RST, RPE, PCHL, JPE, XCHG, CPE, CALL, XRI, RST,
-    RP,  POP, JP,  DI,   CP,  PUSH, ORI, RST, RM,  SPHL, JM,  EI,   CM,  CALL, CPI, RST,
+ // x0    x1    x2    x3    x4    x5    x6    x7    x8    x9    xA    xB    xC    xD    xE    xF
+    NOP,  LXI,  STAX, INX,  INR,  DCR,  MVI,  RLC,  NOP,  DAD,  LDAX, DCX,  INR,  DCR,  MVI,  RRC, // 0x
+    NOP,  LXI,  STAX, INX,  INR,  DCR,  MVI,  RAL,  NOP,  DAD,  LDAX, DCX,  INR,  DCR,  MVI,  RAR, // 1x
+    NOP,  LXI,  SHLD, INX,  INR,  DCR,  MVI,  DAA,  NOP,  DAD,  LHLD, DCX,  INR,  DCR,  MVI,  CMA, // 2x
+    NOP,  LXI,  STA,  INX,  INR,  DCR,  MVI,  STC,  NOP,  DAD,  LDA,  DCX,  INR,  DCR,  MVI,  CMC, // 3x
+    MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV, // 4x
+    MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV, // 5x
+    MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV, // 6x
+    MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  HLT,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV,  MOV, // 7x
+    ADD,  ADD,  ADD,  ADD,  ADD,  ADD,  ADD,  ADD,  ADC,  ADC,  ADC,  ADC,  ADC,  ADC,  ADC,  ADC, // 8x
+    SUB,  SUB,  SUB,  SUB,  SUB,  SUB,  SUB,  SUB,  SBB,  SBB,  SBB,  SBB,  SBB,  SBB,  SBB,  SBB, // 9x
+    ANA,  ANA,  ANA,  ANA,  ANA,  ANA,  ANA,  ANA,  XRA,  XRA,  XRA,  XRA,  XRA,  XRA,  XRA,  XRA, // Ax
+    ORA,  ORA,  ORA,  ORA,  ORA,  ORA,  ORA,  ORA,  CMP,  CMP,  CMP,  CMP,  CMP,  CMP,  CMP,  CMP, // Bx
+    RNZ,  POP,  JNZ,  JMP,  CNZ,  PUSH, ADI,  RST,  RZ,   RET,  JZ,   JMP,  CZ,   CALL, ACI,  RST, // Cx
+    RNC,  POP,  JNC,  OUT,  CNC,  PUSH, SUI,  RST,  RC,   RET,  JC,   IN,   CC,   CALL, SBI,  RST, // Dx
+    RPO,  POP,  JPO,  XTHL, CPO,  PUSH, ANI,  RST,  RPE,  PCHL, JPE,  XCHG, CPE,  CALL, XRI,  RST, // Ex
+    RP,   POP,  JP,   DI,   CP,   PUSH, ORI,  RST,  RM,   SPHL, JM,   EI,   CM,   CALL, CPI,  RST, // Fx
+    
 };
 
 void cycle(I8080* cpu) {
 
     cpu->opcode = (cpu->memory)[cpu->program_counter];
-    
+
     dispatch[cpu->opcode](cpu);
 
 
